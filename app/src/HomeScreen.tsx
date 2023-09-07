aaa4be9fa4203f81fcb886c1578493b708b7d87f
MasterOC <benedikt.oskar.weyer@gmail.com>
2023-09-07 13:24:21 +0200

    adding prebuild

--- app/src/HomeScreen.tsx
+++ app/src/HomeScreen.tsx
@@ -1,14 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { ScrollView, Text, View } from 'react-native';
 import WDILCard from './WDILCard';
+import { MMKVLoader, useMMKVStorage } from 'react-native-mmkv-storage';
+
+const storage = new MMKVLoader().initialize();
 
 const HomeScreen = () => {
+    const [question1, setQuestion1] = useMMKVStorage('question1', storage, 'clean');
+    
     return (
 		<View className="flex-1 justify-start bg-[#F5EFB9] py-3 px-2">
             <StatusBar style="auto" />
 
             <ScrollView>
-                <WDILCard question='clean the kitchen' timeSinceEvent='4 days' />
+                <WDILCard question={question1} timeSinceEvent='4 days' />
 
                 <WDILCard question='sweap the bathroom floor' timeSinceEvent='2 days' />
 
